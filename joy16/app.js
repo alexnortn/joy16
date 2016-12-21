c8b752faa314008e3bbf6301b0b8446de595ce61
Alex Norton <alexnortn@gmail.com>
2016-12-21 01:46:23 -0500

    added node_modules to the party

--- joy16/app.js
+++ joy16/app.js
@@ -14,6 +14,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// All environments
+var PORT = process.env.PORT || 2700;
+
+app.set('port', PORT);
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
